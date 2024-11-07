34a2844bc48d634e2b9da7b9aca00105dbde45bb
Anaele1 <157758390+Anaele1@users.noreply.github.com>
2024-11-07 12:39:06 +0100

    user_input.py
     assignment

---
+++ user_input.py
@@ -0,0 +1,15 @@
+
+
+# user_input.py
+
+# Ask the user for their name and store it in the "name" variable
+name = input("Please enter your name: ")
+
+# Ask the user for their age and store it in the "age" variable
+age = int( input("Please enter you age: "))
+
+# Ask the user for their location and store it in the "location" variable
+location = input("Please enter your location: ")
+
+# Print out a personalized message using the user's input
+print(f"Hello {name}, you are {age} years old and live in {location}.")
